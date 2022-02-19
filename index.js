218c06b64b61fcac1027f0ca92e425053523c32d
Zeshan Shahid <98495821+ZeshanShahid79@users.noreply.github.com>
2022-02-19 19:52:40 +0100

    add js functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+import navigation from './scripts/nav.js';
+
+navigation();
+
+const quizCard = document.querySelectorAll('[data-js="quizCard"]');
+
+quizCard.forEach(card => {
+  const bookmark = card.querySelector('[data-js="bookmark"]');
+  const answerButton = card.querySelector('[data-js="answerButton"]');
+  const answerText = card.querySelector('[data-js="answerText"]');
+
+  bookmark.addEventListener('click', () => {
+    bookmark.classList.toggle('marked');
+  });
+
+  answerButton.addEventListener('click', () => {
+    answerText.classList.toggle('hidden');
+  });
+});
---
+++ scripts/nav.js
@@ -0,0 +1,69 @@
+export default navigation;
+function navigation() {
+  const mainHome = document.querySelector('[data-js="home"]');
+  const mainBookmarks = document.querySelector('[data-js="bookmarks"]');
+  const mainCreate = document.querySelector('[data-js="create"]');
+  const mainProfile = document.querySelector('[data-js="profile"]');
+  //const navbar = document.querySelector('[data-js="navbar"]');
+
+  const buttonHome = document.querySelector('[data-js="buttonHome"]');
+  const buttonBookmarks = document.querySelector('[data-js="buttonBookmarks"]');
+  const buttonCreate = document.querySelector('[data-js="buttonCreate"]');
+  const buttonProfile = document.querySelector('[data-js="buttonProfile"]');
+
+  //icon sets
+  const iconHomeWhite = document.querySelector('[data-js="iconHomeWhite"]');
+  const iconHomeBlack = document.querySelector('[data-js="iconHomeBlack"]');
+  const iconBookmarkWhite = document.querySelector(
+    '[data-js="iconBookmarkWhite"]'
+  );
+  const iconBookmarkBlack = document.querySelector(
+    '[data-js="iconBookmarkBlack"]'
+  );
+  const iconCreateWhite = document.querySelector('[data-js="iconCreateWhite"]');
+  const iconCreateBlack = document.querySelector('[data-js="iconCreateBlack"]');
+  const iconProfileWhite = document.querySelector(
+    '[data-js="iconProfileWhite"]'
+  );
+  const iconProfileBlack = document.querySelector(
+    '[data-js="iconProfileBlack"]'
+  );
+
+  const iconWhite = [
+    iconHomeWhite,
+    iconBookmarkWhite,
+    iconCreateWhite,
+    iconProfileWhite,
+  ];
+  const iconBlack = [
+    iconHomeBlack,
+    iconBookmarkBlack,
+    iconCreateBlack,
+    iconProfileBlack,
+  ];
+
+  const buttons = [buttonHome, buttonBookmarks, buttonCreate, buttonProfile];
+  const mainPage = [mainHome, mainBookmarks, mainCreate, mainProfile];
+
+  buttons.forEach(function (button, index) {
+    button.addEventListener('click', () => {
+      click(mainPage[index]);
+    });
+  });
+
+  function click(aktion) {
+    mainPage.forEach(function (button, index) {
+      if (button == aktion) {
+        aktion.classList.remove('hidden');
+
+        iconWhite[index].classList.add('hidden');
+        iconBlack[index].classList.remove('hidden');
+      } else {
+        button.classList.add('hidden');
+
+        iconWhite[index].classList.remove('hidden');
+        iconBlack[index].classList.add('hidden');
+      }
+    });
+  }
+}
